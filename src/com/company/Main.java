814934f96fad3b3baad389fcd6979fada36e4d43
chernykh.a.n@edu.mirea.ru <N3s_d45o823*b1a>
2021-09-21 12:01:16 +0300

    task1.6.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import static java.util.Arrays.sort;
 public class Main {
-
+    public static int fact(int numb) {
+        int f=1;
+        for (int i=2; i<=numb; i++) {
+            f*=i;
+        }
+        return f;
+    }
     public static void main(String[] args) {
-	// write your code here
+        int[] array;
+        int size;
+        do {
+            array = new int[(int) ((Math.random()) * 10)];
+        } while (array.length==0);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random()*100);
+            System.out.println(array[i]);
+        }
+        System.out.println("\n");
+        sort(array, 0, array.length);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
