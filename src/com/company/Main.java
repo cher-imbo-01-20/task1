bded56c6d4cdde81de934a9205ad4eb8b0e099d8
chernykh.a.n@edu.mirea.ru <N3s_d45o823*b1a>
2021-10-02 22:36:21 +0300

    all done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
-
 import static java.util.Arrays.sort;
+import java.util.Random;
 public class Main {
     public static int fact(int numb) {
         int f=1;
@@ -8,28 +8,50 @@ public class Main {
             f*=i;
         }
         return f;
-    }
-    public static void task6() {
-        int[] array;
+    } //task7
+    public static void task6_1() {
+        int[] array1;
         int size;
         do {
-            array = new int[(int) ((Math.random()) * 10)];
-        } while (array.length==0);
-        for (int i=0; i<array.length; i++) {
-            array[i] = (int)(Math.random()*100);
-            System.out.println(array[i]);
+            array1 = new int[(int) ((Math.random()) * 10)];
+        } while (array1.length==0);
+        for (int i=0; i<array1.length; i++) {
+            array1[i] = (int)(Math.random()*100);
+            System.out.println(array1[i]);
         }
         System.out.println("\n");
-        sort(array, 0, array.length);
+        sort(array1, 0, array1.length);
         int sum=0;
-        for (int var : array) {
+        for (int var : array1) {
             sum+=var;
             System.out.println(var);
         }
         System.out.println("\n");
         System.out.println(sum);
-    }
+    }//+task3
+    public static void task6_2() {
+        int[] array2;
+        int size;
+        Random rnd = new Random();
+        array2 = new int[(rnd.nextInt(20))];
+        for (int i=0; i<array2.length; i++) {
+            array2[i] = rnd.nextInt(100);
+            System.out.println(array2[i]);
+        }
+        System.out.println("\n");
+        sort(array2, 0, array2.length);
+        int sum=0;
+        for (int var : array2) {
+            sum+=var;
+            System.out.println(var);
+        }
+        System.out.println("\n");
+        System.out.println(sum);
+    }//+task3
     public static void main(String[] args) {
-       task6();
+       task6_1();
+       System.out.println("\n");
+       task6_2();
+       System.out.println("\n"+fact(10));
     }
 }
